4ecedafb4560f5a8744b883f418b61c0d9591a59
Rafa <Rafa@RAFA>
2017-02-24 19:13:50 -0300

    Alteração para arquivo de configuração

---
+++ ControladorServicos/Infra/Geral/DadosAcesso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControladorServicos.Infra.Geral
+{
+    public class DadosAcesso
+    {
+        public string Servidor { get; set; }
+        public string BancoDeDados { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
--- ControladorServicos/Infra/Utils.cs
+++ ControladorServicos/Infra/Utils.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using ControladorServicos.Infra.Geral;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
 
 namespace ControladorServicos.Infra
 {
@@ -16,6 +20,54 @@ namespace ControladorServicos.Infra
             StreamReader sr = new StreamReader(System.AppDomain.CurrentDomain.BaseDirectory.ToString()+"/config.txt");
             
             return sr.ReadToEnd().Contains(nomeServicoVerificacao);
+        }
+
+        public DadosAcesso LerConfiguracao()
+        {
+            string arquivo = getArquivo();
+            DadosAcesso dados = new DadosAcesso();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(arquivo);
+            dados.Servidor = xml.SelectSingleNode("/Configuracao/Servidor/Endereco").InnerText;
+            dados.BancoDeDados = xml.SelectSingleNode("/Configuracao/Servidor/BancoDeDados").InnerText;
+            dados.Usuario = xml.SelectSingleNode("/Configuracao/Servidor/Usuario").InnerText;
+            dados.Senha = xml.SelectSingleNode("/Configuracao/Servidor/Senha").InnerText;
+
+            ValidarArquivo();
+        }
+
+        public void ValidarArquivo()
+        {
+            string arquivo = getArquivo();
+
+            if (!File.Exists(arquivo))
+            {
+                throw new Exception();
+            }
+            
+            DadosAcesso dados = new DadosAcesso();
+            XmlDocument xml = new XmlDocument();
+            dados.Servidor = xml.SelectSingleNode("/Configuracao/Servidor/Endereco").InnerText;
+            dados.BancoDeDados = xml.SelectSingleNode("/Configuracao/Servidor/BancoDeDados").InnerText;
+            dados.Usuario = xml.SelectSingleNode("/Configuracao/Servidor/Usuario").InnerText;
+            dados.Senha = xml.SelectSingleNode("/Configuracao/Servidor/Senha").InnerText;
+
+            if(String.IsNullOrEmpty(dados.Servidor) || String.IsNullOrEmpty(dados.BancoDeDados) || 
+                String.IsNullOrEmpty(dados.Usuario) || String.IsNullOrEmpty(dados.Senha))
+            {
+
+            }
+
+            }
+        }
+
+        private static string getArquivo()
+        {
+            string arquivo = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase) + Path.DirectorySeparatorChar + "conexao.xml";
+            arquivo = arquivo.Substring(6, arquivo.Length - 6);
+
+            return arquivo;
         }
     }
 }
