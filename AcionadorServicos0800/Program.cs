e35b2a7f55751da8d40a7e8dd87abc2393be4d04
Rafa <rafaell.rochaa@gmail.com>
2020-05-25 13:24:35 -0300

    1 - Primeiro coment

--- AcionadorServicos0800/Program.cs
+++ AcionadorServicos0800/Program.cs
@@ -10,6 +10,7 @@ namespace AcionadorServicos0800
         static void Main()
         {
             ServiceBase[] ServicesToRun;
+            // Teste commit 1
             ServicesToRun = new ServiceBase[]
             {
                 new Agilus0800()
